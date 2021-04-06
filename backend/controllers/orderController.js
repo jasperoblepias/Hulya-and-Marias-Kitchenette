5f9a1ad0aac14d2e735e9d1e907f71a5c9dcb554
jasperlance <jasperlance.oblepias.iics@ust.edu.ph>
2021-04-06 14:15:33 +0800

    change email admin

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -61,7 +61,8 @@ exports.newOrder = catchAsyncErrors(async(req, res, next) => {
 
     await sendEmailAdmin({
         // change to admin email (hulyaandmarias@gmail.com)
-        email: 'hulyaandmarias@gmail.com',
+        // email: 'hulyaandmarias@gmail.com',
+        email: 'sazon.levi19@gmail.com',
         subject: "Hulya and Maria's Kitchenette: New Order",
         msg
     })
